5a100b6afb2ef1b6f75e677792ab61a85aaa59a1
casualshay <akshaysprabhakar@gmail.com>
2022-01-05 09:28:29 -0800

    Raw layout for screen 1 done

--- src/App.js
+++ src/App.js
@@ -3,8 +3,29 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
+    <div>
       <hr />
+      <div className="welcome">
+        <h5>Welcome</h5>
+        <h3 style={{ marginTop: '-15px' }}>John</h3>
+      </div>
+      <div>
+        <button> Book your coach </button>
+      </div>
+      <div>
+        <h5>Today's Activities</h5>
+        <div>
+          <button>Walk for 30 minutes</button>
+          <button>Only eat between 12pm and 8pm</button>
+          <button>Track meals in MyFitnessPal</button>
+        </div>
+      </div>
+      <div>
+        <p>Home</p>
+        <p>Calendar</p>
+        <p>Profile</p>
+        {/* navbar */}
+      </div>
     </div>
   );
 }
